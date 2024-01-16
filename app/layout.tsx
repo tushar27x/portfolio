34e87be7491d753db0ee52df6ea310102dd847cf
Dev-Tushar27 <sharma.tushar.271003@example.com>
2024-01-16 22:54:08 +0530

    designed navbar structure with responsive design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { ThemeProvider } from './ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <ThemeProvider>
+        <Navbar />
         {children}
+        <Footer />
+        </ThemeProvider>
       </body>
     </html>
   )
