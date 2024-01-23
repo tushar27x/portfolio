b753aaaeadef84d1b38bf0fe2e23efff3a575727
Dev-Tushar27 <sharma.tushar.271003@example.com>
2024-01-23 22:16:49 +0530

    created projects section and redesigned navbar

--- app/components/Projects/page.tsx
+++ app/components/Projects/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
-
+"use client"
+import React from 'react';
+import { useThemeContext } from '@/app/ThemeProvider';
+import Image from 'next/image';
+import Reveal from '../Reveal';
+import Link from 'next/link';
+import PhotoPop from '../PhotoPop';
+import projects from '@/app/projects';
 const Projects = () => {
+  const {displayMode} = useThemeContext()
   return (
-    <div></div>
+    <div className={`${displayMode? 'bg-light text-dark':'bg-dark/90 text-light'} p-[6rem]`}>
+      <div className='flex flex-col items-center gap-[2rem]'>
+        <div>
+          <h1 className='text-3xl font-bold'>Projects</h1>
+        </div>
+        <Reveal>
+          <div className='flex flex-col items-center w-full gap-5 md:flex-row md:gap-[5rem] lg:flex-row lg:gap-[8rem]'>
+            {
+              projects.map((project) =>{
+                return(
+                      <div className='w-full text-center'>
+                  <div className='flex flex-col items-center py-5'>
+                    <Link href={"https://github.com/Tushar27x/airbnb"}>
+                    <div className={`border-[8px] ${displayMode?'border-blue-300':'border-dark/100'} `}>
+                      <PhotoPop>
+                      <Image src={project.imageUrl} width={500} height={200} alt="project-ss" />
+                      </PhotoPop>
+                    </div>
+                    </Link>
+                  </div>
+                  <h3 className='font-semibold text-1xl'>{project.title}</h3>
+                </div>
+                )
+              })
+            }
+        </div>   
+        </Reveal>
+      </div>
+    </div>
   )
 }
 
