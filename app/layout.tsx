81da00c7348900ff735082521a6a171007de1eae
tushar27x <tushar27x@proton.me>
2024-02-06 21:00:47 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer'
 import { ThemeProvider } from './ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
