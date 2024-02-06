079428c64d8f2cdea8f91d6ee44cfe6a524c55a2
Dev-Tushar27 <sharma.tushar.271003@example.com>
2024-02-06 23:03:54 +0530

    .

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Link from 'next/link'
 import React, { useState } from 'react'
-import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 import { useThemeContext } from '../../ThemeProvider';
