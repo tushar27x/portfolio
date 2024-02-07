e5c4d0a958d7120dc8bb616adf37eb707311aee7
tushar27x <tushar27x@proton.me>
2024-02-07 14:01:11 +0530

    added key to map function

--- app/projects.ts
+++ app/projects.ts
@@ -1,13 +1,16 @@
 const projects =[
     {
+        id: 1,
         title: 'Airbnb-clone',
         imageUrl:'/airbnb-clone.png',
     },
     {
+        id: 2,
         title: 'Online Code Editor',
         imageUrl:'/codepen.png',
     },
     {
+        id: 3,
         title: 'Blog Website',
         imageUrl:'/medium-clone-1.png',
     },
